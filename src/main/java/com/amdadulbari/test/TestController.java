1bf773967dd2beae72eb82d2d075e4fc3fa4b095
amdadulbari <amdadulbari@gmail.com>
2021-09-19 00:12:35 +0600

    parameters changed to calculate sum

--- src/main/java/com/amdadulbari/test/TestController.java
+++ src/main/java/com/amdadulbari/test/TestController.java
@@ -1,12 +1,14 @@
 package com.amdadulbari.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
     @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot! -v3";
+    public String index(@RequestParam int firstNumber, @RequestParam int secondNumber) {
+        int sum = firstNumber + secondNumber;
+        return "Sum = "+sum;
     }
 }
